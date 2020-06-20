d7b82654c22d4937ccc025fe1907ea8b853886d2
msadiq074 <46165070+msadiq074@users.noreply.github.com>
2020-06-21 01:45:50 +0530

    Update CRNN_model.py

--- CRNN_model.py
+++ CRNN_model.py
@@ -21,22 +21,16 @@ from keras.activations import relu, sigmoid, softmax
 import keras.backend as K
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
-
 import tensorflow as tf
-
-#ignore warnings in the output
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from tensorflow.python.client import device_lib
-
-# Check all available devices if GPU is available
 print(device_lib.list_local_devices())
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 
 char_list = string.ascii_letters+string.digits
  
 def encode_to_labels(txt):
-    # encoding each output word into digits
     dig_lst = []
     for index, char in enumerate(txt):
         try:
@@ -49,14 +43,12 @@ def encode_to_labels(txt):
 path = '90kDICT32px/'
  
  
-# lists for training dataset
 training_img = []
 training_txt = []
 train_input_length = []
 train_label_length = []
 orig_txt = []
  
-#lists for validation dataset
 valid_img = []
 valid_txt = []
 valid_input_length = []
@@ -71,12 +63,8 @@ flag = 0
 for root, dirnames, filenames in os.walk(path):
  
     for f_name in fnmatch.filter(filenames, '*.jpg'):
-        # read input image and convert into gray scale image
-        #print(root,dirnames,filenames,f_name)
         print(os.path.join(root, f_name))
         img = cv2.cvtColor(cv2.imread(os.path.join(root, f_name)), cv2.COLOR_BGR2GRAY)   
-      #  print(i)
-        # convert each image of shape (32, 128, 1)
         w, h = img.shape
         if h > 128 or w > 32:
             continue
@@ -89,18 +77,14 @@ for root, dirnames, filenames in os.walk(path):
             img = np.concatenate((img, add_zeros), axis=1)
         img = np.expand_dims(img , axis = 2)
         
-        # Normalize each image
         img = img/255.
         
-        # get the text from the image
         txt = f_name.split('_')[1]
         
-        # compute maximum length of the text
         if len(txt) > max_label_len:
             max_label_len = len(txt)
             
            
-        # split the 150000 data into validation and training dataset as 10% and 90% respectively
         if i%10 == 0:     
             valid_orig_txt.append(txt)   
             valid_label_length.append(len(txt))
@@ -114,7 +98,6 @@ for root, dirnames, filenames in os.walk(path):
             training_img.append(img)
             training_txt.append(encode_to_labels(txt)) 
         
-        # break the loop if total data is 150000
         if i == 150000:
             flag = 1
             break
@@ -124,12 +107,9 @@ for root, dirnames, filenames in os.walk(path):
 train_padded_txt = pad_sequences(training_txt, maxlen=max_label_len, padding='post', value = len(char_list))
 valid_padded_txt = pad_sequences(valid_txt, maxlen=max_label_len, padding='post', value = len(char_list))
 
-# input with shape of height=32 and width=128 
 inputs = Input(shape=(32,128,1))
  
-# convolution layer with kernel size (3,3)
 conv_1 = Conv2D(64, (3,3), activation = 'relu', padding='same')(inputs)
-# poolig layer with kernel size (2,2)
 pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
  
 conv_2 = Conv2D(128, (3,3), activation = 'relu', padding='same')(pool_1)
@@ -138,11 +118,9 @@ pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
 conv_3 = Conv2D(256, (3,3), activation = 'relu', padding='same')(pool_2)
  
 conv_4 = Conv2D(256, (3,3), activation = 'relu', padding='same')(conv_3)
-# poolig layer with kernel size (2,1)
 pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
  
 conv_5 = Conv2D(512, (3,3), activation = 'relu', padding='same')(pool_4)
-# Batch normalization layer
 batch_norm_5 = BatchNormalization()(conv_5)
  
 conv_6 = Conv2D(512, (3,3), activation = 'relu', padding='same')(batch_norm_5)
@@ -153,13 +131,11 @@ conv_7 = Conv2D(512, (2,2), activation = 'relu')(pool_6)
  
 squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
  
-# bidirectional LSTM layers with units=128
 blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(squeezed)
 blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(blstm_1)
  
 outputs = Dense(len(char_list)+1, activation = 'softmax')(blstm_2)
 
-# model to be used at test time
 act_model = Model(inputs, outputs)
 
 act_model.summary()
@@ -177,7 +153,6 @@ def ctc_lambda_func(args):
  
 loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([outputs, labels, input_length, label_length])
 
-#model to be used at training time
 model = Model(inputs=[inputs, labels, input_length, label_length], outputs=loss_out)
 
 model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer = 'adam')
@@ -199,16 +174,12 @@ batch_size = 256
 epochs = 15
 model.fit(x=[training_img, train_padded_txt, train_input_length, train_label_length], y=np.zeros(len(training_img)), batch_size=batch_size, epochs = epochs, validation_data = ([valid_img, valid_padded_txt, valid_input_length, valid_label_length], [np.zeros(len(valid_img))]), verbose = 1, callbacks = callbacks_list)
 
-# load the saved best model weights
 act_model.load_weights('best_model.hdf5')
  
-# predict outputs on validation images
 prediction = act_model.predict(valid_img[:10])
  
-# use CTC decoder
 out = K.get_value(K.ctc_decode(prediction, input_length=np.ones(prediction.shape[0])*prediction.shape[1],greedy=True)[0][0])
  
-# see the results
 i = 0
 for x in out:
     print("original_text =  ", valid_orig_txt[i])
